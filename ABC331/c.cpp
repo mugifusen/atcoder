fd869dcf8dcc10f46465c9bca6ad75dda6e5ee1b
fusen <s21t335@kagawa-u.ac.jp>
2024-01-25 15:56:35 +0900

    bが投げれてない、c解答

--- ABC331/c.cpp
+++ ABC331/c.cpp
@@ -6,19 +6,19 @@ int main(void) {
     cin >> N;
     vector<int> A(N), B(N);
     for (int i = 0; i < N; i++) {
-        int sum=0;
+        cin >> A[i];
+    }
+    for (int i = 0; i < N; i++) {
+        int sum = 0;
         for (int j = 0; j < N; j++) {
-            //A[k] > A[i] kはiではないが成立するものsumをbに入れる
-            // mapを用いて上記の条件式に一致するもの以外を0にする。その後sumを取る。
             if (i != j && A[i] < A[j]) {
                 sum += A[j];
             }
         }
-        cout << sum << endl;
         B[i] = sum;
     }
 
-    for (int i = 0; i < B.size(); i++) {
+    for (int i = 0; i < N; i++) {
         cout << B[i] << ' ';
     }
     cout << endl;
---
+++ ABC336/c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(void) {
+    ll n;
+    cin >> n;
+    n--;
+    vector<int> a;
+    // 5進数として考え、N-1番目の値を求め10進数に戻す
+    while(n) {
+        a.push_back(n%5);
+        n /= 5;
+    }
+    if (a.empty()) {
+        a.push_back(0);
+    }
+    reverse(begin(a), end(a));
+    for (auto& x: a) cout << x*2;
+    cout << endl;
+    return 0;
+}
