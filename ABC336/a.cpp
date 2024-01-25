7e042cfcf2f43b7e1ba24cf67a6a1c22cd3e9f58
yasudaTomoaki <s21t335@kagawa-u.ac.jp>
2024-01-25 12:08:03 +0900

    Cまだ

---
+++ ABC336/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    cout << "L";
+    for (int i = 0; i < n; i++) {
+        cout << "o";
+    }
+    cout << "ng" << endl;
+    return 0;
+}
---
+++ ABC336/b.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    // 1/25にABC336のC
+}
