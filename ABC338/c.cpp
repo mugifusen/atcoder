b7a5b15b3d4c9f8fc659b9e98d927da3c1992b31
yasudaTomoaki <s21t335@kagawa-u.ac.jp>
2024-02-03 17:37:30 +0900

    ABC338

--- ABC338/c.cpp
+++ ABC338/c.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
 
 int main(void) {
     int n;
     cin >> n;
     vector<int> q(n), a(n), b(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> q[i];
-    }
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    for (int i = 0; i < n; ++i) {
-        cin >> b[i];
-    }
-    int a_peaple=0, b_peaple=0;
-    for (int i = 0; i < n; ++i) {
-        a_peaple = q[i] - a[i];
-        for (int j = 0; j < n; ++j) {
-            b_peaple = q[j] - b[j];
-            
+    rep(i,n) cin >> q[i];
+    rep(i,n) cin >> a[i];
+    rep(i,n) cin >> b[i];
+    int ans = 0;
+    bool flag = true;
+    for (int x = 0;; ++x) {
+        vector<int> r(n);
+        rep(i,n) {
+            r[i] = q[i] - a[i]*x;
+            if (r[i] < 0) {
+                flag = false;
+                break;
+            }
         }
+        if (!flag) break;
+        int y = 10000000;
+        rep(i,n) {
+            if (b[i] == 0) continue;
+            y = min(r[i]/b[i], y);
+        }
+        ans = max(x+y, ans);
     }
-
+    cout << ans << endl;
+    return 0;
 }
